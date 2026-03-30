2ee62fd91fbb74cf99e2c001e5e73dc4c5f14a39
Priyanshu Sharma <priyanshusharma9998@gmail.com>
2026-03-30 20:59:10 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -78,12 +78,12 @@ export const googleAuth = async (req, res) => {
     
 // }
 
-// export const logOut = async (req,res) => {
-//     try {
-//         await res.clearCookie("token")
-//         return res.status(200).json({message:"LogOut Successfully"})
-//     } catch (error) {
-//          return res.status(500).json({message:`Logout error ${error}`})
-//     }
+export const logOut = async (req,res) => {
+    try {
+        await res.clearCookie("token")
+        return res.status(200).json({message:"LogOut Successfully"})
+    } catch (error) {
+         return res.status(500).json({message:`Logout error ${error}`})
+    }
     
-// }
+}
